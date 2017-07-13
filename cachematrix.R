ca096e06a964187d879945f3cd7c25e1e3e9735a
ppgandhi11 <ppgandhi11@hotmail.com>
2017-07-13 14:57:14 -0400

    Added 2 function definition
    completed func makeCacheMatrix and makeCacheMatrix.
    Also added comments about how to test it with a sample at the bottom of the code.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,54 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  set <- function(y) {
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inver <<- inverse
+  getInverse <- function() inver
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inver <- x$getInverse()
+  
+  if (!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  mat <- x$get()
+  inver <- solve(mat, ...)
+  x$setInverse(inver)
+  inver
+  
 }
+
+## test it with following commands
+## >x<-makeCacheMatrix(matrix(c(1:4),2,2)) # this is the matrix we want to invert
+## >x$get()  # should print the matrix
+## >x$getInverse() # should print NULL first time. should print the "getting cached data" + inverse matrix once cached
+## >cacheSolve(x) # prints inverted matrix
+## >cacheSolve(x)
+##
